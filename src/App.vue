88ffd90504fff58cb40a908aba9c224f5ed5217a
ManoelLucioDev <manoel.programer@gmail.com>
2022-04-28 22:19:07 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <Header />
+
     <router-view/>
   </div>
 </template>
 
+<script>
+
+import Header from './components/Header.vue'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+ 
+  
 }
 
-nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+    <div class="header">
+    <div>
+      <img id=logo alt="logo" src="../assets/logo.svg">
+    </div>
+    <div>
+      <nav>
+        <router-link to="/">Home</router-link> 
+        <router-link to="/products">Catálogo</router-link>
+        <router-link to="/about">Nossa História</router-link>
+      </nav>
+      
+    </div>
+
+  </div>
+  <img src="../assets/shopping-cart.svg" alt="shopping-cart">
+
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 
+    'HeaderComp'
+  
+}
+</script>
+
+<style lang="scss">
+$primary-color: #000D9C;
+$hight-primary-color: #3543da;
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 70px;
+
+nav {
+  
+
+  a {
+    padding-right: 30px ;
+    font-weight: bold;
+    color: $primary-color;
+    text-decoration: none;
+
+  }
+  a:hover {
+      color: $hight-primary-color;
+    }
+}
+
+}
+
+
+</style>
